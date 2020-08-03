a8e533ef7f84257a5d79d94f4ae9e954389eef88
nreal <nreal@makignsense.com>
2020-08-02 21:24:40 -0300

    First commit

---
+++ AliProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AliProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
